7cdccb4cfec895155468d923ab6e0805c4a4c396
jtetart <ladropzonefr@gmail.com>
2019-01-25 23:01:12 +0100

    InventoryManager script and prefab draft + UICanvas prefab + UIManager script first draft

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetInventoryObjectName(int position)
+    {
+        throw new NotImplementedException();
+        //TODO
+        string name = "";
+
+        return name;
+    }
+}
---
+++ Assets/UI/UIManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public enum DialogSpeaker
+    {
+        MAINA,
+        TEDDY
+    }
+
+    [SerializeField]
+    private GameObject dialogPanel;
+    [SerializeField]
+    private Image avatarImage;
+    [SerializeField]
+    private Sprite mainaAvatarSprite;
+    [SerializeField]
+    private Sprite teddyAvatarSprite;
+    [SerializeField]
+    private TMP_Text dialogText;
+
+    private InventoryManager inventoryManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryManager = GameObject.FindObjectOfType<InventoryManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showDialog()
+    {
+        dialogPanel.SetActive(true);
+    }
+
+    public void hideDialog()
+    {
+        dialogPanel.SetActive(false);
+    }
+
+    public void UpdateDialog(DialogSpeaker dialogSpeaker, string text)
+    {
+        switch(dialogSpeaker)
+        {
+            case DialogSpeaker.MAINA:
+                avatarImage.sprite = mainaAvatarSprite;
+                break;
+            case DialogSpeaker.TEDDY:
+                avatarImage.sprite = teddyAvatarSprite;
+                break;
+        }
+        dialogText.text = text;
+    }
+
+    public void ActionInventoryManager(int inventoryPosition)
+    {
+        string objectName = inventoryManager.GetInventoryObjectName(inventoryPosition);
+        switch(objectName)
+        {
+            //TODO
+            default:
+                break;
+        }
+    }
+}
