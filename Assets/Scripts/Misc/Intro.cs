12fed4a96d863f223f86d112380ac99cba489a9d
David Hart <david@atipik.ch>
2019-01-26 22:19:17 +0100

    More text

--- Assets/Scripts/Misc/Intro.cs
+++ Assets/Scripts/Misc/Intro.cs
@@ -9,5 +9,8 @@ public class Intro : MonoBehaviour
         GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.MOTHER, 
             "Tu es une vilaine fille Maïna ! Je t'avais prévenu de ne pas t'aventurer dehors. Pour t'apprendre " +
             "l'obéissance, tu resteras dans ta chambre jusqu'à ta majorité !");
+//        GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.MAINA, 
+//            "Maman est vraiment trop méchante. Si seulement papa était encore là... Il faut que m'échappe pour " +
+//            "le retrouver !");
     }
 }
