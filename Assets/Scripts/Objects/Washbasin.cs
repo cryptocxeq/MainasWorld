cd1ac85f63fa22c4a5aaf98ab0b50efc1717b24f
jtetart <ladropzonefr@gmail.com>
2019-01-27 13:58:55 +0100

    WIP Progress Father talking in imaginary world if clicking on bathroom washbin

---
+++ Assets/Scripts/Objects/Washbasin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Washbasin : Interaction
+{
+    protected override bool PerformAction()
+    {
+        GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.FATHER,
+            "Hello princess. Find me the razor stone and I will tell you where you can find what you are looking for!");
+        return false;
+    }
+
+    protected override bool CanInteract()
+    {
+        return GameManager.Instance.swapper.World == World.Imaginary;
+    }
+}
