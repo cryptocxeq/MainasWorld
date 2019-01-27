9acfcb75b4504718b8782bb46a8f14171c4bae34
jtetart <ladropzonefr@gmail.com>
2019-01-27 14:59:04 +0100

    MotherBedroomDesk behavior script

---
+++ Assets/Scripts/Objects/MotherBedroomDesk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherBedroomDesk : Interaction
+{
+    protected override bool PerformAction()
+    {
+        GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.MAINA,
+           "Yeah! The key to freedom!");
+        EventManager.Instance.ItemPickUp(InventoryManager.BOSS_KEY_GO_NAME);
+        return true;
+    }
+
+    protected override bool CanInteract()
+    {
+        return GameManager.Instance.swapper.World == World.Real;
+    }
+}
