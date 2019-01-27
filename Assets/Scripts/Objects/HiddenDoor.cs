3cf859d0dcb2081d9d69bd1460b8e5805cebca94
jtetart <ladropzonefr@gmail.com>
2019-01-27 14:54:03 +0100

    HiddenDoor script

---
+++ Assets/Scripts/Objects/HiddenDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenDoor : Interaction
+{
+    protected override bool PerformAction()
+    {
+        if (GameManager.Instance.inventory.InventoryObjectOwned(InventoryManager.TORCH_LIGHT_GO_NAME))
+        {
+            this.gameObject.SetActive(true);
+            EventManager.Instance.RevealRoom("SecretPassage");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected override bool CanInteract()
+    {
+        if (GameManager.Instance.inventory.InventoryObjectOwned(InventoryManager.TORCH_LIGHT_GO_NAME))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
