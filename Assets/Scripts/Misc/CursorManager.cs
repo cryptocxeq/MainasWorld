6c599423f00d7741f28e20fa502aacb7504011c5
LAPTOP-JAGF2RL6\issab <issa.barbier@gmail.com>
2019-01-27 10:36:06 +0100

    UI du jeu (dialogue, inventaire et curseur)

--- Assets/Scripts/Misc/CursorManager.cs
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class CursorManager : MonoBehaviour
 {
     [SerializeField] private Texture2D realTexture = null;
+    [SerializeField] private Texture2D realTextureOver = null;
     [SerializeField] private Texture2D imaginaryTexture = null;
+    [SerializeField] private Texture2D imaginaryTextureOver = null;
+
+    World currentWorld;
+    bool currentOver;
 
     void Start()
     {
@@ -13,11 +18,29 @@ public class CursorManager : MonoBehaviour
         EventManager.Instance.OnWorldChange += (World world) => {
             this.UpdateCursor(world);
         };
+
+        EventManager.Instance.OnHighlightChange += (bool value) => {
+            this.UpdateCursor(value);
+        };
     }
 
     private void UpdateCursor(World world)
     {
-        var texture = GameManager.Instance.swapper.World == World.Real ? realTexture : imaginaryTexture;
+        currentWorld = world;
+        UpdateCursor(currentWorld, currentOver);
+    }
+
+    private void UpdateCursor(bool over)
+    {
+        currentOver = over;
+        UpdateCursor(currentWorld, currentOver);
+    }
+
+    private void UpdateCursor(World world, bool over)
+    {
+
+        var texture = GameManager.Instance.swapper.World == World.Real ? (over ? realTextureOver : realTexture) : (over ? imaginaryTextureOver : imaginaryTexture);
         Cursor.SetCursor(texture, Vector2.zero, CursorMode.Auto);
     }
+
 }
