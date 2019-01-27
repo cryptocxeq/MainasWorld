6deb8a663c8d8c17d11778cad8ec6f6f7b6117b9
jtetart <ladropzonefr@gmail.com>
2019-01-27 14:14:57 +0100

    WIP Progress Father talking in imaginary world if clicking on bathroom washbin after finding razor stone

--- Assets/Scripts/Objects/Washbasin.cs
+++ Assets/Scripts/Objects/Washbasin.cs
@@ -6,8 +6,16 @@ public class Washbasin : Interaction
 {
     protected override bool PerformAction()
     {
-        GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.FATHER,
-            "Hello princess. Find me the razor stone and I will tell you where you can find what you are looking for!");
+        if (GameManager.Instance.inventory.InventoryObjectOwned(InventoryManager.RAZOR_STONE_GO_NAME))
+        {
+            GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.FATHER,
+               "Congratulations princess! Here is your clue: I am taller than the tallest building, and my shining light warms out hearts and lights our path.");
+        }
+        else
+        {
+            GameManager.Instance.ui.UpdateDialog(UIManager.DialogSpeaker.FATHER,
+               "Hello princess. Find me the razor stone and I will tell you where you can find what you are looking for!");
+        }        
         return false;
     }
 
