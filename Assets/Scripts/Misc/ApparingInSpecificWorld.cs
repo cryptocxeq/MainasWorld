3ab1c2803a45e0ec87e4d93ebd7ee9a66fef9f3e
LAPTOP-JAGF2RL6\issab <issa.barbier@gmail.com>
2019-01-27 09:38:59 +0100

    Avencement de la map + préparation pour rivière

---
+++ Assets/Scripts/Misc/ApparingInSpecificWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApparingInSpecificWorld : MonoBehaviour
+{
+    [SerializeField] World world;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.Instance.OnWorldChange += (World w) => { GetComponent<Collider2D>().enabled = world == w; };
+    }
+
+}
