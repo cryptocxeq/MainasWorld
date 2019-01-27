f50243da319a61aa280c03809f942165677ecbb7
LAPTOP-JAGF2RL6\issab <issa.barbier@gmail.com>
2019-01-27 16:17:29 +0100

    MotherWindow

---
+++ Assets/Scripts/Objects/MotherWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherWindow : Interaction
+{
+    [SerializeField] string roomName = null;
+    [SerializeField] Transform targetPos = null;
+
+    protected override bool PerformAction()
+    {
+        PlayerController player = GameManager.Instance.player;
+
+        player.transform.position = targetPos.position;
+        EventManager.Instance.RevealRoom(roomName);
+
+        return false;
+    }
+}
